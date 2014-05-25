7ace93e19e2387a064476ff3deee2a3ced90678a
Ariel Melendez <ariel.l.melendez@gmail.com>
2014-05-25 02:30:08 -0400

    Committing the Week 1 assignment materials.

--- Week 1/src/edu/vuum/mooca/SynchronizedQueue.java
+++ Week 1/src/edu/vuum/mooca/SynchronizedQueue.java
@@ -220,21 +220,39 @@ public class SynchronizedQueue {
             // initialization below to create two Java Threads, one
             // that's passed the producerRunnable and the other that's
             // passed the consumerRunnable.
-            Thread consumer = null;
-            Thread producer = null;
+            // ALM - DONE
+            Thread consumer = new Thread(consumerRunnable);
+            Thread producer = new Thread(producerRunnable);
 
             // TODO - you fill in here to start the threads. More
             // interesting results will occur if you start the
             // consumer first.
+            // ALM - DONE
+            // ALM - Convenience boolean provided to change start order of threads
+            boolean bStartConsumerFirst = true;
+            if(bStartConsumerFirst)
+            {
+            	consumer.start();
+            	producer.start();
+            }
+            else
+            {
+            	producer.start();
+            	consumer.start();
+            }
             
             // Give the Threads a chance to run before interrupting
             // them.
             Thread.sleep(100);
 
             // TODO - you fill in here to interrupt the threads.
+            consumer.interrupt();
+            producer.interrupt();
 
             // TODO - you fill in here to wait for the threads to
             // exit.
+            consumer.join();
+            producer.join();
             
             // Do some sanity checking to see if the Threads work as
             // expected.
